0121e15a73f6bd03e0ff3d0985f7bf20b8627319
Jose Alvaro Garcia <jag040393@gmail.com>
2016-11-11 22:57:05 -0800

    Added heroku requirements

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ var request = require('request');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 7015));
 app.use(express.static(process.cwd() + '/public'));
 
 
@@ -28,13 +29,12 @@ app.use(bodyParser.urlencoded({
 	extended: false
 }));
 
-var exphbs = require('express-handlebars');
+
 app.engine('jsx', require('express-react-views').createEngine());
 app.set('view engine', 'jsx');
 
 
-var routes = require('./controllers/cats_controller.js');
+var routes = require('./controllers/articles_controller.js');
 app.use('/', routes);
 
-var port = 3000;
-app.listen(port);
+
